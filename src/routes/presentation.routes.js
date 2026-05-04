da395f2f0af19e486b9e3fa7a2d6af7b30067b26
Juan Jose Estrada Velez <juanjo.jjev@gmail.com>
2026-05-04 11:09:17 -0500

    fix file middleware name

--- src/routes/presentation.routes.js
+++ src/routes/presentation.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { PresentationController } from '../controllers/presentation.controller.js'
 import { authMiddleware } from '../middleware/authtoken.js'
-import { pdfMiddleware } from '../middleware/PDF.middleware.js'
+import { pdfMiddleware } from '../middleware/pdf.middleware.js'
 import { textMiddleware } from '../middleware/text.middleware.js'
 
 export const presentationRoutes = Router()
